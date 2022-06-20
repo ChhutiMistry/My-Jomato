4593f284796078ddfa2a47763069509935df9522
chhuti.mistry <chhuti246@gmail.com>
2022-06-20 20:54:51 +0530

    Commit

--- src/Component/Order/ViewOrder.js
+++ src/Component/Order/ViewOrder.js
@@ -46,10 +46,7 @@ class ViewOrder extends Component {
                }
           }
           let email = sessionStorage.getItem('userInfo').split(',')[1];
-          setTimeout((Waiting, 5000) => {
-               axios.get(`${url}?email=${email}`).then((res) => {this.setState({orders:res.data})})
-          }, timeout);
-          // axios.get(`${url}?email=${email}`).then((res) => {this.setState({orders:res.data})})
+          axios.get(`${url}?email=${email}`, {timeout: 5}).then((res) => {this.setState({orders:res.data})})
      }
 }
 
